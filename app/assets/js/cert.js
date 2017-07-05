3b4a7fdd8defd75b8eb2589917469a781bdfb4b1
Emmanuel Lodovice <name3anad@gmail.com>
2017-07-06 01:15:52 +0800

    refactored code to be more reusable

--- app/assets/js/cert.js
+++ app/assets/js/cert.js
@@ -1,67 +1,79 @@
-var event_id = '31606688456';
-$(document).ready(activate);
-function activate() {
-    var search = window.location.search;
-    var key = '?order=';
-    var index = search.indexOf(key);
-    if (index >= 0) {
-        var orderId = decodeURIComponent(search.substring(index + key.length));
+var cert = {
+    initialize: function() {
+        var _this = this;
+        var eventName = this._getQueryParameter('event');
+        if (!eventName) {
+            window.location = '/';
+        }
+        this.eventName = eventName;
+        var orderId = this._getQueryParameter('order');
+        if (!orderId) {
+            window.location = '/?event=' + eventName;
+        }
         orderId = orderId.replace('#', '');
-        handleEventBriteCert(orderId);
-    } else {
-        key = '?id=';
-        index = search.indexOf(key);
-        if (index >= 0) {
-            var id = decodeURIComponent(search.substring(index + key.length));
-            handleWalkInCert(id);
-        }   else {
-            redirect();
-        }
-    }
-}
-
-function displayCert(name) {
-    $('#name').text(name);
-    $('img').removeClass('hidden');
-    window.print();
-    window.location = '/';
-}
-
-function handleEventBriteCert(orderId) {
-    var apiEndPoint = 'https://www.eventbriteapi.com/v3/orders/' + orderId +
-        '/?token=' + 'MA54QS2FTNFGVCOEJ3HJ';
-
-    $.ajax({
-        url: apiEndPoint,
-        type: 'GET',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('Authorization', 'Bearer MA54QS2FTNFGVCOEJ3HJ');
-        },
-        success: function(data) {
-            if (data.event_id === event_id) {
-                displayCert(data.name);
-            }   else {
-                window.location = '/?error=Invalid Order';
+        var eventInfoPromise = this._getEventInfo(eventName);
+        eventInfoPromise.then(function(info) {
+            if (!info) {
+                window.location = '/';       
             }
-        },
-        error: function(e) {
-            window.location = '/?error=Invalid Order';
-        }
-    });
-}
+            _this.event = info;
+            var eventId = info.eventbrite_id;
+            _this._getEventBriteInfo(eventId, orderId);
+        });
 
-function handleWalkInCert(id) {
-    var ref = window.firebase.database().ref('walk-in/' + id);
-    ref.once('value').then(function(data) {
-        var name = data.val();
-        if (name !== null) {
-            displayCert(name);
-        } else {
-            redirect();
-        }
-    });
-}
+    },
 
-function redirect() {
-    window.location = '/';
-}
+    _getQueryParameter: function(name) {
+        var url = window.location.search;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        var results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    },
+
+    _getEventInfo: function(eventName) {
+        return new Promise(function(resolve, reject) {
+            var ref = window.firebase.database().ref('events/' + eventName);
+            ref.once('value').then(function(data) {
+                var info = data.val();
+                resolve(info);
+            });
+        });
+    },
+
+    _getEventBriteInfo: function(eventbriteId, orderId) {
+        var _this = this;
+        var apiEndPoint = 'https://www.eventbriteapi.com/v3/orders/' + orderId +
+            '/?token=' + 'MA54QS2FTNFGVCOEJ3HJ';
+        $.ajax({
+            url: apiEndPoint,
+            type: 'GET',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer MA54QS2FTNFGVCOEJ3HJ');
+            },
+            success: function(data) {
+                if (data.event_id == eventbriteId) {
+                    _this._displayCert(data.name);
+                }   else {
+                    window.location = '/?event=' + _this.eventName + '&error=Invalid Order';
+                }
+            },
+            error: function(e) {
+                window.location = '/?event=' + _this.eventName + '&error=Invalid Order';
+            }
+        });
+    },
+
+    _displayCert: function(name) {
+        $('#name').text(name);
+        $('#name').css({'top': this.event.offset_top + 'px'});
+        $('img').attr('src', '/assets/img/cert/' + this.event.cert_name);
+        $('img').removeClass('hidden');
+        window.print();
+        window.location = '/?event=' + this.eventName;
+    }
+
+};
+cert.initialize();
--- app/assets/js/login.js
+++ app/assets/js/login.js
@@ -1,7 +1,63 @@
-var search = window.location.search;
-var key = '?error=';
-var index = search.indexOf(key);
-if (index >= 0) {
-    var errorMessage = decodeURIComponent(search.substring(index + key.length));
-    $('.error').text(errorMessage).removeClass('hidden');
-}
+var home = {
+    initialize: function() {
+        var eventName = this._getQueryParameter('event');
+        this._handleError();
+        if (!eventName) {
+            this._showInvalidEventError();
+            return;
+        }
+        this.eventName = eventName;
+        var eventInfoPromise = this._getEventInfo(eventName);
+        var _this = this;
+        eventInfoPromise.then(function(data) {
+            _this._updatePageWithInfo(data);
+        });
+    },
+
+    _updatePageWithInfo: function(eventInfo) {
+        if (!eventInfo) {
+            this._showInvalidEventError();
+            return;
+        }
+        $('.loading').addClass('hidden');
+        $('.main-content img').attr('src', '/assets/img/logo/' + eventInfo.logo_name);
+        var form = $('.main-content form');
+        $('.main-content input[name="event"]').val(this.eventName);
+        $('.main-content').removeClass('hidden');
+    },
+
+    _getQueryParameter: function(name) {
+        var url = window.location.search;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        var results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    },
+
+    _handleError: function() {
+        var errors = this._getQueryParameter('error');
+        if (errors) {
+            $('#error-container').text(errors).removeClass('hidden');
+        }
+    },
+
+    _getEventInfo: function(eventName) {
+        return new Promise(function(resolve, reject) {
+            var ref = window.firebase.database().ref('events/' + eventName);
+            ref.once('value').then(function(data) {
+                var info = data.val();
+                resolve(info);
+            });
+        });
+    },
+
+    _showInvalidEventError: function() {
+        $('.loading').addClass('hidden');
+        $('.main-content').addClass('hidden');
+        $('.error').removeClass('hidden');
+    }
+};
+
+home.initialize();
