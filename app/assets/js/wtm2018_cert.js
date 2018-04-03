5d009ac4f34f16ab73b57f30e6a7dfe62c2ccded
Emmanuel Lodovice <name3anad@gmail.com>
2018-04-04 02:41:13 +0800

    Added wtm cert page

---
+++ app/assets/js/wtm2018_cert.js
@@ -0,0 +1,37 @@
+$(document).ready(activate);
+
+
+function activate() {
+    var search = window.location.search;
+    var key = '?email=';
+    var index = search.indexOf(key);
+    if (index < 0) {
+        redirect();
+    }
+    var email = decodeURIComponent(search.substring(index + key.length));
+    if (email === '') {
+        redirect();
+    }
+
+    email = email.replace('.', '__dot__');
+
+    var ref = window.firebase.database().ref('wtm2018/participants/' + email);
+
+    ref.once('value').then(function(info) {
+        var value = info.val();
+        if (value !== null) {
+            $('#name').text(value);
+            $('img').removeClass('hidden');
+            window.print();
+            redirect();
+        } else {
+            redirect();
+        }
+    });
+
+
+    function redirect() {
+        window.location = '/wtm2018.html';
+    }
+}
+
